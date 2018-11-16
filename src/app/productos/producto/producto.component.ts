2aab6e84bc313ffd3563e537febfd4571467cfb2
Jeferson Herrera <jeferson21029913@gmail.com>
2018-11-16 04:32:46 -0500

    Productos

--- src/app/productos/producto/producto.component.ts
+++ src/app/productos/producto/producto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductoService } from 'src/app/shared/producto.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-producto',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: ProductoService) { }
 
   ngOnInit() {
+    this.resetForm();
   }
 
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.resetForm();
+    this.service.formData = {
+      codigo:null,
+      descripcion:'',
+      detalle:'',
+      precio:null,
+      stock:null,
+      marca:null,
+      categoria:null
+    }
+
+}
 }
